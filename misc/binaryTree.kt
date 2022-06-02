b5305fa6290c1dbaa48ab92c95a6c25e360b4ba5
andystoys <64394640+andystoys@users.noreply.github.com>
2022-06-01 18:15:18 -0700

    Update and rename traversal.kt to binaryTree.kt

--- misc/binaryTree.kt
+++ misc/binaryTree.kt
@@ -4,24 +4,35 @@ data class Node(
     var right: Node? = null
 )
 
-// Inorder
-// l,c,r
-fun inOrder(root: Node) {
+// remember values via closures
+
+// in, pre, post relationships
+
+// bfs
+
+
+// T: O(n), S: O(h) h = height of tree, all dfs
+// Inorder: l,c,r
+fun inOrderR(root: Node) {
     root.left?.let { left -> inOrder(left) }
     root.value?.let { value -> println(value) }
     root.right?.let { right -> inOrder(right) }
 }
+
+fun inOrderI(root: Node) {
+    
+}
+
  
-// Preorder: c,l,r: Time Complexity O(n), visits each node once
-fun preOrder(root: Node) {
+// Preorder: c,l,r
+fun preOrderR(root: Node) {
     root.value?.let { value -> println(root.value) }
     root.left?.let { left -> preOrder(left) }
     root.right?.let { right -> preOrder(right) }
 }
 
-// Postorder
-// l,r,c
-fun postOrder(root: Node) {
+// Postorder: l,r,c
+fun postOrderR(root: Node) {
     root.left?.let { left -> postOrder(left) }
     root.right?.let { right -> postOrder(right) }
     root.value?.let { value -> println(root.value) }
@@ -33,12 +44,12 @@ fun driver() {
          Node(1, Node(2))
     )
     val t2 = Node(1, Node(2, Node(4), Node(5)), Node(3))
-    //preOrder(t1)
-    //preOrder(t2)
-    //inOrder(t1)
-    //inOrder(t2)
-    //postOrder(t1)
-    //postOrder(t2)
+    //preOrderR(t1)
+    //preOrderR(t2)
+    //inOrderR(t1)
+    //inOrderR(t2)
+    //postOrderR(t1)
+    //postOrderR(t2)
 }
 
 fun main() {
